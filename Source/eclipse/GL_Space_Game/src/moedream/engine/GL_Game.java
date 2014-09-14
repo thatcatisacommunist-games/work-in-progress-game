6d1fc195bccf7d9393bd1834e0663d9775e82177
Member1221 <TheisCHR@live.dk>
2014-09-15 01:14:54 +0200

    Changed some directories, and did some cleaning. (And moved silly code, to the silly place. xD)

---
+++ Source/eclipse/GL_Space_Game/src/moedream/engine/GL_Game.java
@@ -0,0 +1,85 @@
+package moedream.engine;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import moedream.engine.drawmisc.SillyStuff;
+import moedream.engine.drawmisc.SimpleDraw;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+
+
+
+public class GL_Game 
+{
+	//PROGRAM MAIN VOID.
+	public static void main(String[] args)
+	{
+		initScr();
+		initGL();
+		gameLoop();
+		trashRemove();
+	}
+	
+	
+	
+	
+	//Remove Trash.
+	private static void trashRemove() 
+	{
+		Display.destroy();
+	}
+
+	
+	//Initiate OpenGL.
+	private static void initGL() 
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0,Display.getWidth(), 0, Display.getHeight(), 2, -1);
+		glMatrixMode(GL_MODELVIEW);
+		
+		glClearColor(0,0,0,1);
+		
+		glDisable(GL_DEPTH_TEST);
+	}
+
+	//Prepare screen.
+	private static void initScr()
+	{
+		try
+		{
+			Display.setDisplayMode(new DisplayMode(500,500));
+			Display.setResizable(true);
+			Display.create();
+			Display.setTitle("Space Game WIP Stuff - Engine begginning.");
+		}
+		catch (LWJGLException ex)
+		{
+			Logger.getLogger(GL_Game.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+	
+	
+
+
+	
+	//The game loop.
+	private static void gameLoop()
+	{
+		while(!Display.isCloseRequested())
+		{
+			//Drawing Loop
+			GameEngine.Draw();
+			
+			
+			//Logic loop
+			GameEngine.Update();
+			
+		}		
+	}
+}
